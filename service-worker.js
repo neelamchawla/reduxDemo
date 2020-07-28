120ff7cb32c2def9e03dbd6c21e7e82365848268
neelamchawla <neelamchawla55@gmail.com>
2020-07-28 16:17:33 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reduxDemo/precache-manifest.c84bd2cb39460156fabae568dbb406cb.js"
+  "/reduxDemo/precache-manifest.c2b0d3479a1539cb1da849f8d439db16.js"
 );
 
 self.addEventListener('message', (event) => {
