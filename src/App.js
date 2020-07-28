3426f7d2e1a4985107120c51e0e6c4de4e3b479f
neelamchawla <neelamchawla55@gmail.com>
2020-07-28 15:53:08 +0530

    updated favicon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import { Link, Route } from 'react-router-dom';
+//Match was used in older version, Now Route is used
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Home';
+// import GamesPage from './component/GamesPage';
+// import GamesForm from './component/GamesForm';
+import { HelloWorld } from './Demo/Demo';
+import Todolist from './TodoList/Todolist';
+import Dashboard from './Dashboard.js/Dashboard';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="ui container">
+          <div className="ui four item menu">
+            <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/">Home</NavLink>
+            <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/dashboard">Dashboard</NavLink>
+            <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/todolist">Todolist</NavLink>
+            <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/demo">Demo</NavLink>
+            {/* <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/games">Games</NavLink>
+            <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/games/new">New Games</NavLink> */}
+          </div>
+          <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/todolist" component={Todolist} />
+          <Route path="/demo" component={HelloWorld} />
+          {/* <Route exact path="/games" component={GamesPage} />
+          <Route path="/games/new" component={GamesForm} /> */}
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
+
+// Menu.item will not change the automatically until you set props active=true and true value should be depend on current router url.
+
+
+// <Route path="/500" component={GamesPage} /> 
+// Match -> pattern, Route -> path 
+
+
+// <Menu stackable>
+//   <Menu.Item active={this.isActive('/')} as={NavLink} exact to='/' content='Home' />
+//   <Menu.Item active={this.isActive('/abc')} as={NavLink} exact to='/abc' content='ABC' />
+//   <Menu.Item active={this.isActive('/def')} as={NavLink} exact to='/def' content='DEF' />
+// </Menu>
+
+// before:  <Link className="item" to="/games/new">New Games</Link>
+//after:    <NavLink className="item" activeclassname = "active" activeonlywhenexact="true" exact to="/games/new">New Games</NavLink>
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './component/redux/rootReducer';
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+        )
+    );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
