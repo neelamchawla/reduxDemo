3b477f4e3699023a4c32584928c4d53e2d3ab1ce
neelamchawla <neelamchawla55@gmail.com>
2020-07-28 15:12:57 +0530

    Updates

---
+++ precache-manifest.c1529c6427b8ec469b15648ae49e7e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097226b544f73b2d1780a7c7681ca172",
+    "url": "/reduxDemo/index.html"
+  },
+  {
+    "revision": "4be4ef43566f2f5b8b48",
+    "url": "/reduxDemo/static/css/main.7ef688eb.chunk.css"
+  },
+  {
+    "revision": "0b8bd85ea8b834cea212",
+    "url": "/reduxDemo/static/js/2.c169927c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reduxDemo/static/js/2.c169927c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be4ef43566f2f5b8b48",
+    "url": "/reduxDemo/static/js/main.a75fa5f2.chunk.js"
+  },
+  {
+    "revision": "01169ed4b8fc4bb1f83c",
+    "url": "/reduxDemo/static/js/runtime-main.b899f766.js"
+  }
+]);
